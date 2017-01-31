6ed31c33cf5fbdd557e14aa72f4d56af829d723f
MrFricker <switzerland@outlook.co.uk>
2017-01-31 23:12:24 +0000

    minor formating

--- src/DatabaseWrapper.php
+++ src/DatabaseWrapper.php
@@ -38,27 +38,23 @@ class DBWrapper extends \PDO implements InterfaceDatabaseWrapper {
     }
 
     public function delete($table, $where, $limit) {
-	if(!isValidTable($table)) {
+        if(!isValidTable($table)) {
     		return false;
     	}
     }
 
     public function update($table, $data, $where, $limit) {
-	if(!isValidTable($table)) {
+        if(!isValidTable($table)) {
     		return false;
     	}
     }
 
     public function fetch($table, $columns, $where, $limit, $order) {
-	if(!isValidTable($table)) {
+	   if(!isValidTable($table)) {
     		return false;
     	}
     }
 
-    public function run($query) {
-	if(!isValidTable($table)) {
-    		return false;
-    	}
     }
 
     private function isValidTable($table) {
