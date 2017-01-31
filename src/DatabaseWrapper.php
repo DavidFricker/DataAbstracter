4b1fc8f6e1388a781aad0452d0dc8b6747c6e502
MrFricker <switzerland@outlook.co.uk>
2017-01-31 23:11:41 +0000

    supports more characters

--- src/DatabaseWrapper.php
+++ src/DatabaseWrapper.php
@@ -8,6 +8,7 @@ class DBWrapper extends PDO implements InterfaceDatabaseWrapper {
 
     private $options = array(
 	            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
 	            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 	            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 	            PDO::ATTR_PERSISTENT => true
