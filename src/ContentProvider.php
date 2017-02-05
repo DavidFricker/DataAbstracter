d0dd368b29926729f8e711dbc060eb93dd75ca0f
MrFricker <switzerland@outlook.co.uk>
2017-02-05 23:27:35 +0000

    todo

--- src/ContentProvider.php
+++ src/ContentProvider.php
@@ -1,6 +1,8 @@
 <?php
 namespace \DavidFricker\DataAbstracter;
-
+/*
+    MAKE THIS A SINGELTON OBJECT SO THAT WE DONT NEED TO USE UGLY DEPENDACY INJECTION
+ */
 // connection should be made in constructer
 class ContentProvider implements InterfaceContentProvider {
     public function __construct() {
