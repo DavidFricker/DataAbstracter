20582c7f82eecb5f9554b97ca8999bf00667a134
MrFricker <switzerland@outlook.co.uk>
2017-02-06 08:42:38 +0000

    todo complete - make class singelton

--- src/ContentProvider.php
+++ src/ContentProvider.php
@@ -5,7 +5,18 @@ namespace \DavidFricker\DataAbstracter;
  */
 // connection should be made in constructer
 class ContentProvider implements InterfaceContentProvider {
-    public function __construct() {
-    	
+     public static function init() {
+        static $instance = null;
+
+        if ($instance === null) {
+            $instance = new ContentProvider();
+        }
+
+        return $instance;
     }
+
+    /**
+     * Set a private __construct to stop instaces being created leading to more than one instance of this class
+     */
+    private function __construct() {}
 }
