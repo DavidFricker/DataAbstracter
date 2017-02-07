29ea600c9960bb25127c53a297bcc5e345ff8b6f
MrFricker <switzerland@outlook.co.uk>
2017-02-07 23:03:01 +0000

    example usage

--- example/example1.php
+++ example/example1.php
@@ -1,14 +1,32 @@
 <?php
-include 'InterfaceDatabaseWrapper.php';
-include 'MySQLDatabaseWrapper.php';
-include 'ContentContract.php';
+include '..\src\interfaces\InterfaceDatabaseWrapper.php';
+include '..\src\interfaces\InterfaceContentProvider.php';
+include '..\src\adapter\MySQLDatabaseWrapper.php';
+include '..\src\contract\ContentContract.php';
+include '..\src\contract\TableContentContractUsers.php';
+include '..\src\provider\AppContentProvider.php';
 
-use DavidFricker\DataAbstracter\InterfaceDatabaseWrapper;
-use DavidFricker\DataAbstracter\MySQLDatabaseWrapper;
-use DavidFricker\DataAbstracter\ContentContract;
+use DavidFricker\DataAbstracter\Interfaces\InterfaceDatabaseWrapper;
+use DavidFricker\DataAbstracter\Adapter\MySQLDatabaseWrapper;
+use DavidFricker\DataAbstracter\Contract\ContentContract;
+use DavidFricker\DataAbstracter\Contract\TableContentContractUsers;
+use DavidFricker\DataAbstracter\Provider\AppContentProvider;
 
-$DBWrapper = new MySQLDatabaseWrapper();
+$AppContentProvider = AppContentProvider::init();
+
+//var_dump($AppContentProvider->isValidInput(TableContentContractUsers::TABLE_NAME, TableContentContractUsers::COL_ID, '1'));
+
+$R=$AppContentProvider->insert(TableContentContractUsers::TABLE_NAME, [TableContentContractUsers::COL_EMAIL => 'test@example.com']);
+$R=$AppContentProvider->getLastInsertID();
+#$R=$AppContentProvider->delete(TableContentContractUsers::TABLE_NAME, [TableContentContractUsers::COL_ID => $R], 1);
+#$R=$AppContentProvider->fetch(TableContentContractUsers::TABLE_NAME, [TableContentContractUsers::COL_EMAIL], [TableContentContractUsers::COL_ID => $R], 1);
+$AppContentProvider->update(TableContentContractUsers::TABLE_NAME, [TableContentContractUsers::COL_EMAIL => 'UPDATE@example.com'], [TableContentContractUsers::COL_ID => $R], 1);
+$R=$AppContentProvider->fetch(TableContentContractUsers::TABLE_NAME, [TableContentContractUsers::COL_EMAIL], [TableContentContractUsers::COL_ID => $R], 1);
+var_dump($R);
+
 /*
+$DBWrapper = new MySQLDatabaseWrapper();
+
 $result = $DBWrapper->insert(ContentContract::TABLE['customers'], [ContentContract::SCHEMA['customers']['email'] => 'dbf@dbf.com']);
 if ($result === false) {
 	echo 'Error: '.$DBWrapper->getLastError();
