20084776d4f0e0daadf32774089232211668769d
MrFricker <switzerland@outlook.co.uk>
2017-02-07 23:06:01 +0000

    new custom exception for missing controller
    thrown by the Router

---
+++ src/exception/InvalidControllerException.php
@@ -0,0 +1,17 @@
+<?php
+namespace DavidFricker\Router\Exception;
+
+use \Exception;
+
+/**
+  * A wrapper around a DB driver to expose a uniform interface
+  *
+  * Bassically an abstraction over the complexity of the PDO class, but by design this could wrap any strctured storage mechanism 
+  * A database engine adapter
+  *
+  * @param string $myArgument With a *description* of this argument, these may also
+  *    span multiple lines.
+  *
+  * @return void
+  */
+class InvalidControllerException extends Exception {}
