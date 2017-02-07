37e364baba72afd6851c10699c403c13e38f0ff1
MrFricker <switzerland@outlook.co.uk>
2017-02-07 23:06:26 +0000

    split classes into seperate class files

--- src/contract/ContentContract.php
+++ src/contract/ContentContract.php
@@ -1,9 +1,53 @@
 <?php
+
 namespace DavidFricker\DataAbstracter\Contract;
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-echo 1;
+use DavidFricker\DataAbstracter\Contract\TableContentContractUsers;
+
+// declare final class?
+class ContentContract {
+    const DATA_TYPE_INT = 'int';
+    const DATA_TYPE_TEXT = 'text';
+    const DATA_TYPE_DATETIME = 'datetime';
+    
+    // holds pairs of the table alias and 
+    public $tables = [];
+    public $validators = [];
+
+    /**
+     * Set a private __construct to stop instaces being created leading to more than one instance of this class
+     */
+    public function __construct() {
+        $this->registerValidators();
+        $this->registerTables();
+    }
+
+    private function registerTables() {
+        $this->tables[TableContentContractUsers::TABLE_NAME] = new TableContentContractUsers();
+    }
+
+    private function registerValidators() {
+        $this->validators[self::DATA_TYPE_INT] = function($input) {
+            return is_int($input);
+        };
+
+        $this->validators[self::DATA_TYPE_TEXT] = function($input) {
+            return true;
+        };
+
+        $this->validators[self::DATA_TYPE_DATETIME] = function($input) {
+            return true;
+        };
+    }
+}
+
+
+
+
+
+//var_dump(TableContentContractUsers::COL_ID, $ContentContract->tables[TableContentContractUsers::TABLE_NAME]);
+
+
 /*
 
 // declare final class?
@@ -54,61 +98,3 @@ class ContentContract {
 
 var_dump(ContentContract::TABLE['customers'], ContentContract::SCHEMA['customers']['id'], ContentContract::TYPE['customers']['id']);
 */
-
-// declare final class?
-class ContentContract {
-    const DATA_TYPE_INT = 'int';
-    const DATA_TYPE_TEXT = 'text';
-    const DATA_TYPE_DATETIME = 'datetime';
-    
-    // holds pairs of the table alias and 
-    public $tables = [];
-    public $validators = [];
-
-    /**
-     * Set a private __construct to stop instaces being created leading to more than one instance of this class
-     */
-    public function __construct() {
-        $this->registerValidators();
-        $this->registerTables();
-    }
-
-    private function registerTables() {
-        $this->tables[TableContractUsers::TABLE_NAME] = new TableContractUsers();
-    }
-
-    private function registerValidators() {
-        $this->validators[self::DATA_TYPE_INT] = function($input) {
-            return is_int($input);
-        };
-
-        $this->validators[self::DATA_TYPE_TEXT] = function($input) {
-            return true;
-        };
-
-        $this->validators[self::DATA_TYPE_DATETIME] = function($input) {
-            return true;
-        };
-    }
-}
-
-// declare final class?
-class TableContractUsers {
-    const TABLE_NAME = 'users';
-
-    const COL_ID = 'customerid';
-    const COL_EMAIL = 'customerid';
-    const COL_PASSWORD = 'customerid';
-
-    const TYPE = [
-        self::COL_ID => ContentContract::DATA_TYPE_INT,
-        self::COL_EMAIL => ContentContract::DATA_TYPE_TEXT,
-        self::COL_PASSWORD => ContentContract::DATA_TYPE_TEXT,
-    ];
-}
-
-$ContentContract = new ContentContract();
-
-var_dump(TableContractUsers::COL_ID, $ContentContract->tables[TableContractUsers::TABLE_NAME]);
-
-
